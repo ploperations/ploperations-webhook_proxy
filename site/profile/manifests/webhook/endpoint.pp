712c485caa9a7ce3d3a55064aabf8b19964a775b
Daniel Parks <dp@puppetlabs.com>
2016-08-06 01:39:01 -0700

    (QENG-3890) Webhook proxy
    This sets up a node to proxy webhook requests from external sources
    though to specific URLs internally.
    
    This enables webhooks to all of our Jenkins instances that require
    maintenance windows, to jenkins-cinext, and to the SysOps Jenkins.
    
    This allows event notification from external sources. For example,
    GitHub can notify Jenkins when a branch is updated, causing a job to run
    immediately rather than needing to poll.

---
+++ site/profile/manifests/webhook/endpoint.pp
@@ -0,0 +1,28 @@
+# An external webhook endpoint
+#
+# If name is set to the internal webhook endpoint, this will automatically
+# generate a path in the form `/$hostname/$url`. For example, the name
+# `'https://jenkins.ops.puppetlabs.net/github-webhook/'` will result in
+# an external endpoint path of `/jenkins.ops.puppetlabs.net/github-webhook/`.
+define profile::webhook::endpoint (
+  Pattern[/^\//] $path = $name.regsubst('^https?://', '/').regsubst('/*$', '/'),
+  Pattern[/^https?:\/\/\w.+\//] $target = $name,
+) {
+  nginx::resource::location { "webhook = ${path}":
+    vhost            => 'webhook',
+    ssl              => true,
+    ssl_only         => true,
+    location         => "= ${path}",
+    proxy            => $target,
+    proxy_redirect   => 'default',
+    proxy_set_header => [
+      'Host               $proxy_host',
+      'X-Real-IP          $remote_addr',
+      'X-Forwarded-For    $proxy_add_x_forwarded_for',
+      'X-Forwarded-Server $host',
+      'X-Forwarded-Host   $host',
+      'X-Forwarded-Proto  $scheme',
+      'X-Forwarded-Ssl    $https',
+    ],
+  }
+}
