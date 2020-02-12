2de749bf40350724436d9d780ccc590a13f15d0c
Gene Liverman <gene.liverman+06301990@puppet.com>
2020-02-12 11:35:08 -0500

    Generalizing to work as a module
    Things that were hard coded in the profile don't make sense in the
    module. Also removed some site-specific code such as profile_metadata
    and supplemental profiles.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,49 +1,15 @@
 # Proxy external webhook endpoints to internal hosts
 class webhook_proxy (
+  Stdlib::Fqdn $cert_fqdn,
+  Array[Stdlib::Fqdn] $jenkins_fqdns = [],
+  Array[Stdlib::Http] $endpoints = [],
   String[1] $canonical_fqdn = $facts['networking']['fqdn'],
+  String[1] $ssl_name = $cert_fqdn,
 ) {
-  profile_metadata::service { $title:
-    human_name        => 'GitHub webhook proxy',
-    owner_uid         => 'daniel.parks',
-    team              => infracore,
-    end_users         => ['discuss-sre@puppet.com'],
-    escalation_period => '24x7',
-    downtime_impact   => "Internal services aren't notfied about repo changes.",
-    other_fqdns       => ['webhook.puppet.com'],
-    notes             => @("NOTES"),
-      This allows Github webhooks to access our internal servers. For example:
-
-      ~~~ puppet
-      profile::webhook::endpoint { 'https://jenkins.puppetlabs.com/github-webhook/': }
-      ~~~
-
-      Creates a webhook proxy so that GitHub can send notifications to the
-      internal Ops Jenkins. The external URL will be
-      `https://webhook.puppet.com/jenkins.puppetlabs.com/github-webhook/`.
-      |-NOTES
-  }
-
-  if $::profile::server::params::fw {
-    include profile::fw::https
-  }
-
-  include profile::nginx
-
-  profile::nginx::redirect { 'default':
-    destination => "https://${canonical_fqdn}",
-    default     => true,
-    ssl         => true,
-  }
-
+  include nginx
   include ssl
 
-  ssl::cert { 'webhook.puppet.com': }
-
-  if $facts['classification']['stage'] == 'prod' {
-    $ssl_name = 'webhook.puppet.com'
-  } else {
-    $ssl_name = 'wildcard.ops.puppetlabs.net'
-  }
+  ssl::cert { $cert_fqdn: }
 
   nginx::resource::server { 'webhook':
     server_name          => [$canonical_fqdn],
@@ -80,39 +46,17 @@ class webhook_proxy (
     location_custom_cfg => { 'return' => '404' },
   }
 
-  # Endpoints for Jenkins instances
-  [
-    'jenkins.ops.puppetlabs.net',
-    'jenkins-compose.delivery.puppetlabs.net',
-    'jenkins.puppetlabs.com',
-    'jenkins-cinext.delivery.puppetlabs.net',
-    'jenkins-master-prod-1.delivery.puppetlabs.net',
-    'cinext-jenkinsmaster-enterprise-prod-1.delivery.puppetlabs.net',
-    'cinext-jenkinsmaster-sre-prod-1.delivery.puppetlabs.net',
-    'jenkins-sre.delivery.puppetlabs.net',
-    'cinext-jenkinsmaster-pipeline-prod-1.delivery.puppetlabs.net',
-    'jenkins-pipeline.delivery.puppetlabs.net',
-    'jenkins-pipeline-dev.delivery.puppetlabs.net',
-  ].each |$host| {
-    profile::webhook::endpoint {
-      # Handle pushes to a branch
-      "https://${host}/github-webhook/": ;
-      # Handle changes to a PR
-      "https://${host}/ghprbhook/": ;
-    }
+  # General endpoints
+  $endpoints.each |$endpoint| {
+    webhook_proxy::endpoint { $endpoint: }
   }
 
-  profile::webhook::endpoint {
-    # Code Manager
-    'https://yoda.puppetlabs.com:8170/code-manager/v1/webhook/': ;
-    'https://puppet.ops.puppetlabs.net:8170/code-manager/v1/webhook': ;
-    'https://pe-infranext-prod.infc-aws.puppet.net:8170/code-manager/v1/webhook': ;
-    'https://pe-infranext-test.infc-aws.puppet.net:8170/code-manager/v1/webhook': ;
-    'http://pe-cd4pe-infranext-prod-1.infc-aws.puppet.net:8000/github/push': ;
-    'http://pe-cd4pe-infranext-test-1.infc-aws.puppet.net:8000/github/push': ;
-    # Argo CD
-    'https://argocd-test.k8s.infracore.puppet.net/api/webhook': ;
-    # Atlantis Endpoint
-    'http://forge-ci-prod-1.ops.puppetlabs.net:4141/events':;
+  # Shortcut for Jenkins instances
+  $jenkins_fqdns.each |$host| {
+    # Handle pushes to a branch
+    webhook_proxy::endpoint { "https://${host}/github-webhook/": }
+
+    # Handle changes to a PR
+    webhook_proxy::endpoint { "https://${host}/ghprbhook/": }
   }
 }
